230df2ec9b117815c0ce88567e255de91c8e342e
MyrtoKy <myrtokyriakides@gmail.com>
2026-04-16 13:30:07 +0300

    Create Player.java

---
+++ src/Player.java
@@ -0,0 +1,26 @@
+public class Player{
+    private Hand hand;
+    private double money;
+    private double bet;
+
+    public Player(double bet){
+        hand = new Hand();
+        money = 100; //all players have a standard buy-in of 100
+        this.bet = bet;
+        money -= bet; 
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+    
+    public void hit(Deck deck){
+        hand.addCard(deck.dealCard());
+        hand.toString();
+        System.out.println("You now have a total of " + hand.getTotalValue());
+    }
+
+    public String stand(){
+        return "You stand with " + hand.getTotalValue();
+    }
+}
